ffe37b04f388706a980cbbf3bb235de9be26625b
Soham Patil <sohammpatil2512@gmail.com>
2026-06-24 00:33:57 +0530

    fix cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,13 +5,18 @@ const postModel = require('./models/post.model')
 const cors = require('cors')
 
 const app = express()
-app.use(cors({
+
+app.use(
+  cors({
     origin: [
       "http://localhost:5173",
       "https://posting-photos-xpuy.vercel.app"
     ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
-}))
+  })
+);
+
 app.use(express.json())
 
 const upload = multer({storage:multer.memoryStorage()})
